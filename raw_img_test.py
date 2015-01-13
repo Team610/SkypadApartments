9adda3e222c299b59e3b5a82977aaad60bafda44
Mike Yan <michael.t.yan@gmail.com>
2015-01-12 19:00:40 -0500

    Simplest 2-camera code

---
+++ raw_img_test.py
@@ -0,0 +1,16 @@
+import cv2
+import time
+import numpy as np
+
+fps = 30 #frames per second
+
+camera_feed_0 = cv2.CaptureCamera(0)
+camera_feed_1 = cv2.CaptureCamera(0)
+
+while (1):
+	#Read camera
+	ret0, frame0 = camera_feed_0()
+	ret1, frame1 = camera_feed_1()
+
+	#System wait/delay
+	time.sleep(1/fps) #in seconds
